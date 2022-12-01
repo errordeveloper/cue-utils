d3deb67a1ac64c202b4476c6706b8821bdfe0a85
Ilya Dmitrichenko <errordeveloper@gmail.com>
2022-12-01 12:03:28 +0000

    Handle error list

--- errors/errors.go
+++ errors/errors.go
@@ -10,7 +10,11 @@ import (
 
 func Describe(desc string, err error) error {
 	w := bytes.NewBuffer(nil)
-	errors.Print(w, err, &errors.Config{Cwd: "/"})
+
+	for _, err := range errors.Errors(err) {
+		errors.Print(w, err, &errors.Config{Cwd: "/"})
+	}
+
 	msg, err := io.ReadAll(w)
 	if err != nil {
 		return err
