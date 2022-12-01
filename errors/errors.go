d9ab3cabca9c106d7e1cc6a58a6abde03a33d702
Ilya Dmitrichenko <errordeveloper@gmail.com>
2022-12-01 12:26:19 +0000

    Revert "Handle error list"
    This reverts commit d3deb67a1ac64c202b4476c6706b8821bdfe0a85.

--- errors/errors.go
+++ errors/errors.go
@@ -10,11 +10,7 @@ import (
 
 func Describe(desc string, err error) error {
 	w := bytes.NewBuffer(nil)
-
-	for _, err := range errors.Errors(err) {
-		errors.Print(w, err, &errors.Config{Cwd: "/"})
-	}
-
+	errors.Print(w, err, &errors.Config{Cwd: "/"})
 	msg, err := io.ReadAll(w)
 	if err != nil {
 		return err
