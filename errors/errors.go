33e074618503fc77c726631396ceae6853ac07cb
Ilya Dmitrichenko <errordeveloper@gmail.com>
2022-12-01 12:40:16 +0000

    Revert "Improve `errors.Describe`"
    This reverts commit 6c6ad00e49baa9e9840e514babd01af95c1bbb53.

--- errors/errors.go
+++ errors/errors.go
@@ -1,19 +1,11 @@
 package errors
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 
 	"cuelang.org/go/cue/errors"
 )
 
 func Describe(desc string, err error) error {
-	w := bytes.NewBuffer(nil)
-	errors.Print(w, err, &errors.Config{Cwd: "/"})
-	msg, err := io.ReadAll(w)
-	if err != nil {
-		return err
-	}
-	return fmt.Errorf("%s: %s", desc, msg)
+	return fmt.Errorf("%s: %s", desc, errors.Details(err, nil))
 }
