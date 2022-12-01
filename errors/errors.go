6c6ad00e49baa9e9840e514babd01af95c1bbb53
Ilya Dmitrichenko <errordeveloper@gmail.com>
2022-12-01 11:54:43 +0000

    Improve `errors.Describe`

--- errors/errors.go
+++ errors/errors.go
@@ -1,11 +1,19 @@
 package errors
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 
 	"cuelang.org/go/cue/errors"
 )
 
 func Describe(desc string, err error) error {
-	return fmt.Errorf("%s: %s", desc, errors.Details(err, nil))
+	w := bytes.NewBuffer(nil)
+	errors.Print(w, err, &errors.Config{Cwd: "/"})
+	msg, err := io.ReadAll(w)
+	if err != nil {
+		return err
+	}
+	return fmt.Errorf("%s: %s", desc, msg)
 }
